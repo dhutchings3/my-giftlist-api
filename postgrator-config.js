175c7ca6809595b03c34ac0ea8548f4e27101e04
Danielle Marie <dhutchings36@yahoo.com>
2019-12-14 06:27:31 -0600

    added files and updated for deployment

---
+++ postgrator-config.js
@@ -0,0 +1,10 @@
+require("dotenv").config();
+
+module.exports = {
+  migrationsDirectory: "migrations",
+  driver: "pg",
+  connectionString:
+    process.env.NODE_ENV === "test"
+      ? process.env.DB_TEST_URL
+      : process.env.DB_URL
+};
