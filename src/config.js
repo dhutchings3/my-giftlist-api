08923d0951b404d81f02d65476d32a1c4834162c
Danielle Marie <dhutchings36@yahoo.com>
2020-03-05 07:47:17 -0600

    testing cors

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@ module.exports = {
   NODE_ENV: process.env.NODE_ENV || "development",
   DATABASE_URL: process.env.DATABASE_URL || 'postgresql://postgres@postgres/my-giftlist',
   TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://postgres@postgres/my-giftlist-test',
-  // CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'https://localhost:300',
-  CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || "https://my-giftlist.now.sh/",
+  CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'https://localhost:3000',
+  // CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || "https://my-giftlist.now.sh/",
   JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
 }
